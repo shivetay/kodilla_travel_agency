7c38a9ea192edb8bf630236b5acc2f3900eafce4
Łukasz Dawidowicz <lddev@outlook.com>
2019-09-30 19:43:35 +0200

    add actions dispatcher from props

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,15 +9,18 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.addTag(tag, checked);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      this.props.removeTag(tag, checked);
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeDuration({[type]: value});
   }
 
   handleSearch(phrase){
