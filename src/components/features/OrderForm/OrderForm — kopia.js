7429ce407c5e66036922e01cc39eae81ce6adb36
Łukasz Dawidowicz <lddev@outlook.com>
2019-10-11 10:53:53 +0200

    bug fix

--- src/components/features/OrderForm/OrderForm — kopia.js
+++ src/components/features/OrderForm/OrderForm — kopia.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import styles from './OrderForm.scss';
-
+import pricing from '../../../data/pricing';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
-import pricing from '../../../data/pricing';
+// import style from './OrderForm.scss';
 
-import {Row, Col} from 'react-flexbox-grid';
+import {Grid, Row, Col} from 'react-flexbox-grid';
+import Section from '../../layout/Section/Section';
 import Button from '../../common/Button/Button';
 import {formatPrice} from '../../../utils/formatPrice';
 import {calculateTotal} from '../../../utils/calculateTotal';
 import settings from '../../../data/settings';
 
-
 const sendOrder = (options, tripCost, tripName, tripId, countryCode) => {
-  const totalCost = formatPrice(calculateTotal(tripCost, options, countryCode));
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
 
   const payload = {
     ...options,
@@ -44,19 +43,9 @@ const sendOrder = (options, tripCost, tripName, tripId, countryCode) => {
 };
 
 
-class OrderForm extends React.Component {
-  onSubmit = (event) => {
-    event.preventDefault();
-  }
-
-  static propTypes = {
-    tripCost: PropTypes.node,
-    options: PropTypes.object,
-  }
-  render(){
-    
-    const {tripCost, options, setOrderOption, tripName, tripId, countryCode} = this.props;
-    return (
+const OrderForm = (props ,tripCost, options, setOrderOption, tripName, tripId, countryCode) => (
+  <Section>
+    <Grid>
       <form onSubmit={() => sendOrder(options, tripCost, tripName, tripId, countryCode)}>
         <Row>
           {pricing.map(pricingData => (
@@ -65,15 +54,19 @@ class OrderForm extends React.Component {
             </Col>
           ))}
           <Col xs={12}>
-            <OrderSummary tripCost={tripCost} options={options}/>
+            <OrderSummary tripCost={props.tripCost} options={props.options} />
             {options.currentValue}
           </Col>
-          <Button>Order now!</Button>
+          <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
         </Row>
       </form>
-      
-    );
-  }
-}
+    </Grid>
+  </Section>
+);
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.node,
+  options: PropTypes.object,
+};
 
 export default OrderForm;
