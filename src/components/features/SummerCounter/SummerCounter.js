a708d00fff55c81d5c698fdc75e0b69d06ca06d7
Łukasz Dawidowicz <lddev@outlook.com>
2019-10-24 13:59:27 +0200

    create summer timer

--- src/components/features/SummerCounter/SummerCounter.js
+++ src/components/features/SummerCounter/SummerCounter.js
@@ -1,25 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './SummerCounter.scss';
+import { settings } from '../../../data/dataStore';
 
 class SummerCounter extends React.Component {
+  static propTypes = {
+    summerDescription: PropTypes.string,
+    noSummer: PropTypes.string,
+    oneDaySummer: PropTypes.string,
+    renderElementFunc: PropTypes.func,
+  }
 
-  //FIXME:
+  static defaultProps = {
+    summerDescription: settings.summerDescription,
+    noSummer: settings.noSummer,
+    oneDaySummer: settings.oneDaySummer,
+  }
+
+
   dayCount(){
     const today = new Date();
     const summer = new Date(today.getUTCFullYear(), 5, 21);
     const oneDay = 1000*60*60*24;
 
-    if(today.getUTCMonth() === 5 && today.getUTCDate() >=21 ) {
+    if(today.getUTCMonth() !== 5 && today.getUTCDate() >21 ) {
       summer.setUTCFullYear(summer.getUTCFullYear()+1);
     }
     return Math.ceil((summer.getTime() - today.getTime())/(oneDay));
+  }
+
+  renderElement(){
+    let renderElementDate = new Date();
+    const summerCounter = this.dayCount();
+
+    if(summerCounter === 1) {
+      renderElementDate = summerCounter  + this.props.oneDaySummer ;
+    } else if (renderElementDate.getUTCMonth() !== 5 && renderElementDate.getUTCDate() === 21 || renderElementDate.getUTCMonth() !== 8 && renderElementDate.getUTCDate() === 23){
+      renderElementDate = this.props.noSummer;
+    } else {
+      renderElementDate = summerCounter + this.props.summerDescription;
+    }
+
+    return renderElementDate;
   }
 
   render() {
     return (
       <div className={styles.component}>
         <h3 className={styles.summerCounter}>
-          {this.dayCount()}
+          {this.renderElement()}
         </h3>
       </div>
     );
--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -1,4 +1,7 @@
 export const settings = {
   title: 'Happy Hour',
   promoDescription: 'Current poromo - 20%',
+  noSummer: '',
+  summerDescription: ' days to summer left!!',
+  oneDaySummer: ' day to summer left!!',
 };
