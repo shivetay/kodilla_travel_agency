2593d05739600002eb0fabd7105288c62e8a8539
Łukasz Dawidowicz <lddev@outlook.com>
2019-10-24 11:38:19 +0200

    add day count

--- src/components/features/SummerCounter/SummerCounter.js
+++ src/components/features/SummerCounter/SummerCounter.js
@@ -3,10 +3,24 @@ import styles from './SummerCounter.scss';
 
 class SummerCounter extends React.Component {
 
+  //FIXME:
+  dayCount(){
+    const today = new Date();
+    const summer = new Date(today.getUTCFullYear(), 5, 21);
+    const oneDay = 1000*60*60*24;
+
+    if(today.getUTCMonth() === 5 && today.getUTCDate() >=21 ) {
+      summer.setUTCFullYear(summer.getUTCFullYear()+1);
+    }
+    return Math.ceil((summer.getTime() - today.getTime())/(oneDay));
+  }
+
   render() {
     return (
       <div className={styles.component}>
-        <h3 className={styles.summerCounter}>Odliczanie</h3>
+        <h3 className={styles.summerCounter}>
+          {this.dayCount()}
+        </h3>
       </div>
     );
   }
